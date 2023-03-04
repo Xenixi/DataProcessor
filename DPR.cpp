15f105788a82059b3eba888cb9e8cb1d47b55e87
Kobe McManus <kobe.mcmanus@xenixi.com>
2023-03-04 15:05:29 -0500

    start

--- DPR.cpp
+++ DPR.cpp
@@ -3,5 +3,13 @@
 
 int main(int argc, char **argv)
 {
-    std::cout << "DPR 1.0: Enter File Name: ";    
+    std::cout << "DPR 1.0: Enter file(s) separated by commas: ";
+    std::string input; 
+
+    std::getline(std::cin, input);
+
+    std::cout << "Attempting to load '" << input << "'" << std::endl;
+
+    
+
 }
